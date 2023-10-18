6d77806ec3c92bedd4ac301fb7c809022d5651fa
OkFrank12 <cfoonyemmemme@gmail.com>
2023-10-18 01:43:20 +0100

    new push

--- utils/googleMail.ts
+++ utils/googleMail.ts
@@ -1,17 +1,33 @@
 import nodemailer from "nodemailer";
 import path from "path";
 import ejs from "ejs";
+import { google } from "googleapis";
 
 const baseURL: string = "http://localhost:5173";
 const baseURL1: string = "http://localhost:1000";
 
+const G_ID =
+  "403139932252-k0ksvgd56ohc39lsckt5bt3oquahgnvb.apps.googleusercontent.com";
+const G_SECRET = "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";
+const G_REFRESH =
+  "1//04bsN5npSCiQqCgYIARAAGAQSNwF-L9Irifs6Ypy-8tdvnhCU0OPHZDjC8st6x82OOKEzVryQnYpRCh6rzl-4DLsGrrkA7var9dI";
+const G_URL = "https://developers.google.com/oauthplayground";
+
+const oAuth = new google.auth.OAuth2(G_ID, G_SECRET, G_URL);
+oAuth.setCredentials({ access_token: G_REFRESH });
+//pass: "cdxv resq qsxp zfev",
 const sendMailToUser = async (user: any, friend: any) => {
   try {
+    const accessToken: any = (await oAuth.getAccessToken()).token;
     const transport = nodemailer.createTransport({
       service: "gmail",
       auth: {
         user: "cfoonyemmemme@gmail.com",
-        pass: "cdxv resq qsxp zfev",
+        type: "OAuth2",
+        clientId: G_ID,
+        clientSecret: G_SECRET,
+        refreshToken: G_REFRESH,
+        accessToken,
       },
     });
 
